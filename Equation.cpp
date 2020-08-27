51dd572c92b924dde47957da27b987246d492e59
19az <70327803+19az@users.noreply.github.com>
2020-08-27 19:54:35 +0300

    Update Equation.cpp

--- Equation.cpp
+++ Equation.cpp
@@ -2,6 +2,7 @@
 #include <math.h>
 #include <assert.h>
 #define INF -1
+#define ROUND_ERROR 0.00001
 
 int square_equation_solve(double a, double b, double c, double* x1, double* x2);
 
@@ -12,7 +13,10 @@ int main()
 
     double a = 0, b = 0, c = 0;
     printf ("# Enter a, b, c:\n");
-    scanf ("%lg %lg %lg", &a, &b, &c);
+    while (scanf ("%lg %lg %lg", &a, &b, &c) != 3)
+        {
+        printf ("Wrong type of data\n");
+        }
 
     double x1 = 0, x2 = 0;
     int number_of_roots = square_equation_solve(a, b, c, &x1, &x2);
@@ -28,7 +32,7 @@ int main()
         case 2: printf ("Equation has 2 root: %g and %g\n", x1, x2);
         break;
 
-        case INF: printf ("Any number\n") ;
+        case INF: printf ("Any number\n");
         break;
 
         default: printf ("Error, equation has %d roots", number_of_roots);
@@ -65,7 +69,7 @@ int square_equation_solve(double a, double b, double c, double* x1, double* x2)
             }
         else
             {
-            *x1 = -c / b;
+            *x1 = - c/b;
             return 1;
             }
         }
@@ -76,18 +80,18 @@ int square_equation_solve(double a, double b, double c, double* x1, double* x2)
 
         if (d == 0)
             {
-            *x1 = -b / (2*a) ;
-            return 1 ;
+            *x1 = - b/(2*a);
+            return 1;
             }
         else if (d < 0)
             {
-            return 0 ;
+            return 0;
             }
         else
             {
-            *x1 = (-b - sqrt(d))/(2*a);
-            *x2 = (-b + sqrt(d))/(2*a);
-            return 2 ;
+            *x1 = (- b - sqrt(d))/(2*a);
+            *x2 = (- b + sqrt(d))/(2*a);
+            return 2;
             }
         }
     }
