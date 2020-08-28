3e55f666cd53732b16082a1d6ce67fc90cc4813c
19az <70327803+19az@users.noreply.github.com>
2020-08-29 00:38:57 +0300

    Add files via upload

---
+++ doc/all_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['equation_2ecpp_0',['Equation.cpp',['../_equation_8cpp.html',1,'']]]
+];
---
+++ doc/files_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['equation_2ecpp_2',['Equation.cpp',['../_equation_8cpp.html',1,'']]]
+];
