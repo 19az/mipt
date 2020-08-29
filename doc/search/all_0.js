efe95a0cdc951ca3cb83bebb4cecff5d62e61ec1
19az <70327803+19az@users.noreply.github.com>
2020-08-29 23:45:00 +0300

    Add files via upload

---
+++ doc/search/all_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['eps_0',['EPS',['../_equation_8cpp.html#a6ebf6899d6c1c8b7b9d09be872c05aae',1,'Equation.cpp']]],
+  ['equation_2ecpp_1',['Equation.cpp',['../_equation_8cpp.html',1,'']]]
+];
---
+++ doc/search/files_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['equation_2ecpp_4',['Equation.cpp',['../_equation_8cpp.html',1,'']]]
+];
